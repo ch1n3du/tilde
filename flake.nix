ce72326bdc4ccf7d5985395f1a2666561f2cb83a
Daniel Onyesoh <danielonyesoh@gmail.com>
2024-09-10 08:31:20 +0100

    Added 'zed-editor', 'gearlever'

--- flake.nix
+++ flake.nix
@@ -12,8 +12,8 @@
     };
   };
 
-  # outputs = { self, nixpkgs, ... }@inputs:
-  outputs = { self, mixrank, nixpkgs, ... }@inputs:
+  outputs = { self, nixpkgs, ... }@inputs:
+  # outputs = { self, mixrank, nixpkgs, ... }@inputs:
   let 
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
@@ -26,7 +26,7 @@
         modules = [
           ./hosts/ebisu/configuration.nix
           inputs.home-manager.nixosModules.default
-          mixrank.nixosModules.dev-machine
+          # mixrank.nixosModules.dev-machine
         ];
       };
     };
